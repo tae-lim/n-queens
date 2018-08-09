198ce9aaffde99ade3f68ef608ebe8a2a652242b
tae-lim <taelim92@gmail.com>
2018-08-09 14:44:04 -0700

    Implemented findRooksSolution

--- src/solvers.js
+++ src/solvers.js
@@ -15,11 +15,74 @@
 
 
 
+//To use the Board() constructor, you must pass it an argument in one of the following formats:
+//An object. To create an empty board of size n:
+//  {n: <num>} - Where <num> is the dimension of the (empty) board you wish to instantiate
+//EXAMPLE: var board = new Board({n:5})
+
+//An array of arrays (a matrix). To create a populated board of size n:
+//[ [<val>,<val>,<val>...], [<val>,<val>,<val>...], [<val>,<val>,<val>...] ] - Where each <val> is whatever value you want at that location on the board    EXAMPLE: var board = new Board([[1,0,0],[0,1,0],[0,0,1]])
+// Board is a class we can use to initiate a new board (n will be between 1-8)
+
+//var board = new Board({'n': n});
+//we can loop through initial board and toggle pieces(+1) for each rows/columns
+//output:
+
+// return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
+
+//if n === 1 -> adjust board the pieces and return toggled solution
+//if n === 2 -> etc...
+
+// I:
+//   [[0, 0, 0],
+//   [0, 0, 0],
+//   [0, 0, 0]]
+// O:
+//   [[1, 0, 0],
+//   [0, 1, 0],
+//   [0, 0, 1]]
+
+//iterate through rows,
+  //toggle piece, if no conflict(hasRowConflictAt AND hasColumnConflictAt)
+
+  // helper function that takes in (board, columns)
+    // Loop through board and grab rows as i
+    // Toggle pieces(i, columns)
+
+
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+
+  var board = new Board({'n': n});
+  var solution = undefined;
+
+  var matrix = function (board, row, length) {
+
+    if (row === length) {
+      return true;
+    }
+
+    for (let i = 0; i < length; ++i) {
+      //debugger;
+      board.togglePiece(row, i);
+      if (!board.hasAnyRooksConflicts()) {
+        if (matrix(board, row + 1, length)) {
+          if (solution === undefined) {
+            solution = board;
+          }
+          return true;
+        }
+      }
+      board.togglePiece(row, i);
+    }
+  }
+
+  matrix(board, 0, n);
+
+  solution = board.rows();
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
+
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
